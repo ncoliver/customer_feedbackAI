785e33de3c060a748e48c0f2d3b13caf3b039388
ncoliver <marques.oliver91@gmail.com>
2025-07-14 06:35:09 -0400

    update branch

--- server.py
+++ server.py
@@ -18,11 +18,22 @@ def sent_analyzer():
 
     # Pass the text to the emotion detector functiont to store the response
     response = emotion_detector(text_to_analyze)
-
+    dominant_emotion = max(dict(response.items()), key=response.get)
+    if dominant_emotion is None:
+        return "Invalid inpute! Try again."
     # Extract 
     return response
-
-
+    emotion_labels = ['anger', 'disgust', 'joy', 'fear', 'sadness', 'dominant']
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    emotion_scores = {}
+    
+    for label in emotion_labels:
+                if label != 'dominant':
+                    score = emotions[label]
+                    emotion_scores[label] = score
+                else:
+                    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+                    emotion_scores['dominant'] = dominant_emotion
 
 @app.route("/")
 def render_index_page():
