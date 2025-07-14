8f8fc2ad248abb7e659f05623713f7b6574d30e6
Marques Oliver <marques.oliver91@gmail.com>
2025-07-14 05:23:08 -0400

    Update emotion_detection.py
    Return statement formatted, code cleaned up

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -9,19 +9,25 @@ def emotion_detector(text_to_analyze):
     response = requests.post(url, json=myobj, headers=header)
     formatted_response = json.loads(response.text)
     
-    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
-    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
-    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
-    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
-    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
-    
-    
-    
-    return {
-        'anger': anger_score, 
-        'disgust': disgust_score, 
-        'fear': fear_score, 
-        'joy': joy_score, 
-        'sadness': sadness_score
-    }
+    emotion_labels = ['anger', 'disgust', 'joy', 'fear', 'sadness', 'dominant']
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    emotion_scores = {}
 
+    for label in emotion_labels:
+        if label != 'dominant':
+            score = emotions[label]
+            emotion_scores[label] = score
+        else:
+            dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+            emotion_scores['dominant'] = dominant_emotion
+
+        
+    return f"""
+    For the given statement, the system response is 
+    'anger': {emotion_scores['anger']},
+    'disgust': {emotion_scores['disgust']}, 
+    'fear': {emotion_scores['fear']}, 
+    'joy': {emotion_scores['joy']}, 
+    'sadness': {emotion_scores['sadness']}. 
+    The dominant emotion is {emotion_scores['dominant']}.
+    """
